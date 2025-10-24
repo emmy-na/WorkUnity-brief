fde75d7b4a1fc87a1cdf90be5b5cf52a679594c2
emmy-na <imanenahmad@gmail.com>
2025-10-24 23:25:21 +0100

    Add files via upload

---
+++ database/seeders/EmployeesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Employee;
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        Employee::factory()->count(50)->create();
+
+        // exemple d'employé réel/admin
+        Employee::create([
+            'first_name' => 'Admin',
+            'last_name'  => 'WorkUnity',
+            'email'      => 'admin@workunity.local',
+            'position'   => 'Administrateur',
+        ]);
+    }
+}

a7312a94be5be0c4cbcd7eb5e828788c29f599c2
emmy-na <imanenahmad@gmail.com>
2025-10-24 23:28:55 +0100

    Add files via upload

---
+++ storage/storage/framework/views/a84166fce1288187f8d21811f998bdd7.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+  </head>
+  <body>
+    <h1 class="text-3xl text-red-500 font-bold underline">
+      Hello world!
+    </h1>
+  </body>
+</html><?php /**PATH C:\Users\HP\Desktop\brief6\workunity-app\resources\views/welcome.blade.php ENDPATH**/ ?>

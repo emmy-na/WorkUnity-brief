162c04c96a2685060c91d5830f2098c46472ace0
emmy-na <imanenahmad@gmail.com>
2025-11-04 23:55:06 +0100

    Update 2025_10_24_213508_create_employees_table.php

--- database/migrations/2025_10_24_213508_create_employees_table.php
+++ database/migrations/2025_10_24_213508_create_employees_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email')->unique();
+            $table->string('position')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('employees');
